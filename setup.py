a49b956e4cd3fde6b8cf52c0367ae76efa5eaa76
comfazil <comfazil@yahoo.com>
2023-08-11 19:02:33 +0530

    first

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hyphen_e_dot = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","")for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+        return requirements
+
+setup(
+    name = 'DiamondPricePrediction',
+    version = '0.0.1',
+    author = 'Fazil',
+    author_email='comfazil@yahoo.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
